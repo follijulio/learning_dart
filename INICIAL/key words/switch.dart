57b947d355cccf6e7aaab6cf7529e49507981b9b
folli14 <cesarfolli@gmail.com>
2024-01-03 23:06:23 -0300

    learning switch case

---
+++ INICIAL/key words/switch.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  int miau = 8;
+  switch (miau) {
+    case 8:
+      print('hello guy');
+      break;
+    default:
+      print('bye guy');
+      break;
+  }
+}
