d9830c40f6160dafcbd87a4755ccec0a78cede38
folli14 <cesarfolli@gmail.com>
2024-02-20 09:28:16 -0300

    inglês

--- IFAL/EXERCICIOS/exercicio08.dart
+++ IFAL/EXERCICIOS/exercicio08.dart
@@ -1,18 +1,18 @@
 import 'dart:io';
 
 void main(List<String> args) {
-  String valor = '';
+  String value = '';
   var number = 0;
-  var i = 1;
-  var alto = 0;
+  var i = 0;
+  var average = 0;
   do {
     print("Digite o $i ª número: ");
-    valor = stdin.readLineSync()!;
-    number = int.tryParse(valor) ?? 0;
-    if (number > alto) {
-      alto = number;
+    value = stdin.readLineSync()!;
+    number = int.tryParse(value) ?? 0;
+    if (number != -1) {
+      average = average + number;
+      i++;
     }
-    i++;
   } while (number != -1);
-  print("O número mais alto é: $alto");
+  print("O número mais alto é: ${average / i}");
 }
