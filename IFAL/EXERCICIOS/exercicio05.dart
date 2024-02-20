b00eff964bce8b54059b9efec052e59135602c32
folli14 <cesarfolli@gmail.com>
2024-02-20 09:27:42 -0300

    inglês

--- IFAL/EXERCICIOS/exercicio05.dart
+++ IFAL/EXERCICIOS/exercicio05.dart
@@ -1,13 +1,13 @@
 void main(List<String> args) {
   print("digite um valor: ");
   print("Digite um outro valor: ");
-  var valor1 = 10;
-  var valor2 = 12;
-  if (valor1 > valor2) {
-    print("O ${valor1} é maior");
-  } else if (valor2 > valor1) {
-    print("O ${valor2} é maior");
+  var value1 = 10;
+  var value2 = 12;
+  if (value1 > value2) {
+    print("O ${value1} é maior");
+  } else if (value2 > value1) {
+    print("O ${value2} é maior");
   } else {
-    print("Os dois valores são iguais ${(valor1 + valor2) / 2}");
+    print("Os dois valores são iguais ${(value1 + value2) / 2}");
   }
 }
