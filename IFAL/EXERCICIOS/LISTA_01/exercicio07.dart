37fe4ea3b283b840cb0d1705b22e035959f7f1ad
folli14 <cesarfolli@gmail.com>
2024-03-03 19:03:36 -0300

    07

--- IFAL/EXERCICIOS/LISTA_01/exercicio07.dart
+++ IFAL/EXERCICIOS/LISTA_01/exercicio07.dart
@@ -1,14 +1,14 @@
 import 'dart:io';
 
 void main(List<String> args) {
-  var low = 1000000000000000000;
+  var low = 0;
   var number = 0;
   for (var i = 1; i <= 10; i++) {
     print("Digite o $i ª número: ");
     number = int.tryParse(stdin.readLineSync()!) ?? 0;
-    if (number < low) {
+    if (number < low || i == 0) {
       low = number;
     }
   }
-  print("O número mais alto foi: $low");
+  print("O número mais baixo digitado foi: $low");
 }
