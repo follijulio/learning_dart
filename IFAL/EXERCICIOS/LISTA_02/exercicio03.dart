e212a002e2843ff2973ee8e4449d95f0ffe8bade
folli14 <cesarfolli@gmail.com>
2024-03-03 19:03:57 -0300

    03

--- IFAL/EXERCICIOS/LISTA_02/exercicio03.dart
+++ IFAL/EXERCICIOS/LISTA_02/exercicio03.dart
@@ -2,15 +2,13 @@ import 'dart:io';
 
 void main(List<String> args) {
   print("Qual o valor de seu salário?: ");
-  double amount_to_pay = calculate(double.parse(stdin.readLineSync()!));
-  print("O valor a ser pago é $amount_to_pay");
+  print(
+      "O valor a ser pago é ${calculate(double.parse(stdin.readLineSync()!))}");
 }
 
 double calculate(double value) {
   double amount_to_pay = 0.0;
-  if (value <= 2000) {
-    amount_to_pay = (value * 0);
-  } else if (value > 2000.01 && value <= 3500) {
+  if (value > 2000.01 && value <= 3500) {
     amount_to_pay = (value * 0.15);
   } else if (value > 3500.01 && value <= 5000) {
     amount_to_pay = (value * 0.22);
